df2ebfa6fe3efaea8ae3d53016d931378c86aba2
Utkarsh Likhar <utkarsh.likhar07@gmail.com>
2026-02-22 13:55:26 +0530

    Add Stone Game solution

---
+++ Arrays/stone_game.cpp
@@ -0,0 +1,15 @@
+// Problem: Stone Game
+// Link: https://leetcode.com/problems/stone-game/
+// Difficulty: Medium
+// Approach: Mathematical Observation
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+// solution: there is 0 pobability of bob wining a game.
+
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
